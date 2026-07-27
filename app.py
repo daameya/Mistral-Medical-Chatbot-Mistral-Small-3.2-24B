c0e8c854b84d04e6ae4b314e1c8257e9c87a16d3
daameya <1998ameya@gmail.com>
2026-07-27 19:35:46 +0530

    app and docker syntax

--- app.py
+++ app.py
@@ -80,4 +80,5 @@ def chat():
 
 # Run App
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
--- dockerfile
+++ dockerfile
@@ -10,4 +10,4 @@ RUN pip install -r requirements.txt
 
 EXPOSE 8000
 
-CMD ["gunicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["gunicorn", "app:app", "--bind", "0.0.0.0:8000"]
