d39bdd8b1788ff6d56e2a32e2744145eb0e9a84f
daameya <1998ameya@gmail.com>
2026-07-27 19:58:51 +0530

    lighter embedding model added

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+#from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from typing import List
 from langchain.schema import Document
 
@@ -45,5 +46,6 @@ def text_split(extracted_data):
 
 #Download the Embeddings from HuggingFace 
 def download_hugging_face_embeddings():
-    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
+    #embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
     return embeddings
